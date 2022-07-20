f4c350a7bb546ff135338eae34b497611aeba422
kuhuisien <>
2022-07-20 14:16:33 +0800

    add general css styles, add header

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<h1>BunchUp</h1>
+</header>
+
+<style>
+	header {
+		position: fixed;
+		width: 100%;
+		top: 0;
+		left: 0;
+		height: 4rem;
+		background: linear-gradient(var(--pure-white) 10.45%, var(--accent-color) 41.35%);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		box-shadow: 0 2px 6px rgb((0, 0), 0, 0.26);
+	}
+
+	h1 {
+		color: var(--pure-white);
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		padding: 4rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0rem auto;
+		box-sizing: border-box;
+	}
+</style>
