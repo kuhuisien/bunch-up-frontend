33faa992ca2fe5017f91b8dae87c3241e7e0e93d
kuhuisien <>
2022-07-20 15:16:46 +0800

    add mock data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
+<script lang="ts">
+	import type { Bunch } from './__types';
+
+	let dataList: Bunch[] = [
+		{
+			id: 'b1',
+			title: 'CSS Bootcamp',
+			subtitle: 'Learn CSS for the first time or brush up your CSS skills and dive in even deeper',
+			description:
+				'we start at the very basics and gradually dive in deeper and deeper. And we do this by showing both practical examples as well as the theory behind it.',
+			imageUrl: 'https://images.app.goo.gl/48BbgqePiL17aUW89',
+			address: 'Suite 104 Block 12 Apartment Pkns Jalan Au3/1 Ampang Ulu Wilayah Persekutuan',
+			email: 'learnCss.outlook.com'
+		},
+		{
+			id: 'b2',
+			title: 'Drawing Workshop',
+			subtitle: 'Pour Art and Ccrylic Painting',
+			description:
+				'With some basic guidance and material provided, you can relieve your stress by learning to draft, sketch, and paint on an empty canvas. ',
+			imageUrl:
+				'https://images.says.com/uploads/story_source/source_image/810150/a9a4.jpg?_ga=2.90453722.1359893855.1658301175-382748050.1658301175',
+			address: '111 Persiaran Cempaka Sari 3 Taman Cempaka',
+			email: 'yuroku.hotmail.com'
+		}
+	];
+</script>
+
 <h1>Welcome</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
